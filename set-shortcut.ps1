b1eebb24751e5cebd38b2cd5d80874b504b1341a
Hasan Berisha <12546489+hberisha1@users.noreply.github.com>
2020-08-11 16:59:24 -0400

    Create set-shortcut.ps1

---
+++ set-shortcut.ps1
@@ -0,0 +1,5 @@
+param ( [string]$SourceExe, [string]$DestinationPath )
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut($SourceExe)
+$Shortcut.TargetPath = $DestinationPath
+$Shortcut.Save()
